f4b9cecb630ed1a5a229d33438a4ca65b9535386
dockingmob <manassehsadik167@gmail.com>
2024-11-11 14:41:50 +0000

    Built SplashScreen

--- lib/Screens/splashsceen.dart
+++ lib/Screens/splashsceen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:hackathon/Components/component_test.dart';
 import 'package:hackathon/constants.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -10,12 +13,27 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 5),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const ComponentTest(),
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: kPrimaryColor,
       body: Center(
         child: Image(
-          image: AssetImage("lib/Assets/Imags/copy1.png"),
+          image: AssetImage("Assets/images/copy1.png"),
+          width: 250,
         ),
       ),
     );
